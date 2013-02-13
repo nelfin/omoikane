4f9ee5dc9f94d54bd11b447bc2b3cbe7b63ab7de
Andrew Haigh <nelfin@gmail.com>
2013-02-13 17:32:29 +1100

    Restructure code as single module

--- omoikane.py
+++ omoikane.py
@@ -1,6 +1,39 @@
 
 import time
+
+import bs4
 import requests
+
+class SousChef(object):
+    """Organise underlings to determine column values."""
+    def __init__(self):
+        self.soup = None
+        self.callbacks = dict()
+
+    def soupify(self, handle):
+        """Produce a tag soup from a HTML file."""
+        self.soup = bs4.BeautifulSoup(handle)
+
+    def open(self, filename):
+        """Open an HTML file and process it."""
+        with open(filename) as html_file:
+            self.soupify(html_file)
+
+    def rowify(self, matcher):
+        """Find sampled portions of tag soup."""
+        return matcher(self.soup)
+
+    def register(self, **callbacks):
+        """Add one or more callbacks by keyword arguments."""
+        self.callbacks.update(callbacks)
+
+    def process(self, row_pattern):
+        """TODO: magic."""
+        rows = self.rowify(row_pattern)
+        for row in rows:
+            columns = dict((key, f(row))
+                           for key, f in self.callbacks.iteritems())
+
 
 class DeskClerk(object):
     """
@@ -30,3 +63,5 @@ class DeskClerk(object):
             self.submit(form, data)
             if count < len(repeat_values) - 1:
                 time.sleep(self.cooldown)
+
+
